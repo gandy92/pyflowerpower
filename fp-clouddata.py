b5e163611356894ddf180d721f787d92fac53c22
gandy92 <gandy92@googlemail.com>
2016-09-11 21:17:54 +0200

    Add debug switch for conrolling verbosity

--- fp-clouddata.py
+++ fp-clouddata.py
@@ -10,6 +10,9 @@ import sys
 
 SCRIPT = 'fp-clouddata.py v1.0'
 
+debug = 0
+
+
 def read_sensor_data_file(filename):
     params = {}
     data = []
@@ -33,11 +36,12 @@ def read_sensor_data_file(filename):
 
     return params, data, head
 
+
 sens_id = sys.argv[1]
 filename = 'hist-%s.dat' % sens_id
 params, data, head = read_sensor_data_file(filename)
-print(pformat(params))
-# print(pformat(data))
+2 <= debug and print(pformat(params))
+5 <= debug and print(pformat(data))
 startup = int(params['current time']) - int(params['sensor time'])
 last_ts = startup + int(params['lts'])
 first_ts = last_ts - int(params['sp'])*(len(data)-1)
@@ -65,7 +69,7 @@ def fetch_cloud_samples(sens_id, from_ts, to_ts):
         print("Could not find location with sensor %s. Bailing out." % sens_id)
         return ()
 
-    print(location_id)
+    3 <= debug and print(location_id)
     i = 0
     # cloud sample timestamps are in utc
     ts_1 = datetime.utcfromtimestamp(from_ts)
@@ -78,7 +82,7 @@ def fetch_cloud_samples(sens_id, from_ts, to_ts):
         req = requests.get('https://apiflowerpower.parrot.com/sensor_data/v2/sample/location/' + location_id,
                            headers=a_header, params={'from_datetime_utc': ts_1,
                                                      'to_datetime_utc': ts_2})
-        # print('Server response: \n {0}'.format(pformat(req.json())))
+        4 <= debug and print('Server response: \n {0}'.format(pformat(req.json())))
         ts_1 = ts_2
         for sample in req.json()['samples']:
             cts = sample['capture_ts']
@@ -90,12 +94,7 @@ def fetch_cloud_samples(sens_id, from_ts, to_ts):
 
 
 cdata = fetch_cloud_samples(sens_id, first_ts-450, last_ts+450)
-print(pformat(cdata))
-
-print('sensor data ranges from %d to %d' % (first_ts,last_ts))
-
-print('sensor data ranges from %s to %s' % (str(datetime.fromtimestamp(first_ts)),
-                                            str(datetime.fromtimestamp(last_ts)) ))
+3 <= debug and print(pformat(cdata))
 
 f = open('comp-%s.dat' % sens_id, 'w')
 f.write('# Cloud samples collected by %s\n' % SCRIPT)
@@ -105,16 +104,14 @@ f.write('#timestamp idx h0 h1 h2 h3 h4 h5 air_temp par vwc\n')
 for ts in sorted(cdata.keys()):
     idx = int((ts - first_ts) / int(params['sp']) + 0.5)
     if 0 <= idx < len(data):
-        print(ts, idx, ' '.join(map(str, data[idx])), ' '.join(map(str, cdata[ts])))
+        2 <= debug and print(ts, idx, ' '.join(map(str, data[idx])), ' '.join(map(str, cdata[ts])))
         f.write('%d\t%d\t%s\t%s\n' % (ts, idx, '\t'.join(map(str, data[idx])), '\t'.join(map(str, cdata[ts]))))
 
 f.close()
 
-print('sensor data ranges from %d to %d' % (first_ts,last_ts))
+print('sensor data ranges starts at %s (ts=%d)' % (str(datetime.fromtimestamp(first_ts)), first_ts))
+print('                 and ends at %s (ts=%d)' % (str(datetime.fromtimestamp(last_ts)), last_ts))
 
-print('sensor data ranges from %s to %s' % (str(datetime.fromtimestamp(first_ts)),
-                                            str(datetime.fromtimestamp(last_ts)) ))
-
-print(datetime.utcnow())
-print(datetime.now())
+1 <= debug and print(datetime.utcnow())
+1 <= debug and print(datetime.now())
 
